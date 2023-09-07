4412e8852966bbd9fe5bdf0fc70ec1970d3d07b9
silenc3502 <gcccompil3r@gmail.com>
2023-09-08 02:00:27 +0900

    update: Add exception and remove resource blocking [MSS-7]

--- transmitter/tx.py
+++ transmitter/tx.py
@@ -24,7 +24,7 @@ class Transmitter:
         return self.pid
 
     def transmit_command(self, client_socket, client_address, pid_queue):
-        print("transmit_command client_address: ", client_address)
+        print(f"transmit_command client_address: {client_address}, client_socket: {client_socket}")
         pid_queue.put(("tx", os.getpid()))
 
         def sigint_transmit_handler(signum, frame):
@@ -36,32 +36,34 @@ class Transmitter:
         signal.signal(signal.SIGUSR1, sigint_transmit_handler)
 
         while True:
-            with client_socket:
-                try:
-                    from_fastapi_data = socket_server_queue.get()
-                    print("from fastapi data: ", from_fastapi_data[0], from_fastapi_data[1])
-                    # data = self.transmitter_command_data_queue.get()
-                    if from_fastapi_data[0] is None:
-                        time.sleep(0.5)
-                        continue
-
-                    if isinstance(from_fastapi_data, tuple):
-                        string_data = ' '.join(map(str, from_fastapi_data))
-                        print('{} command transmit [{}] from {}'.format(dt.now(), string_data, client_address[0]))
-                        client_socket.sendall(string_data.encode())
-                    else:
-                        print("Something wrong in command")
-
-                    exit()
-
-                except queue.Empty:
+            # with client_socket:
+            try:
+                from_fastapi_data = socket_server_queue.get(False)
+                print("from fastapi data: ", from_fastapi_data[0], from_fastapi_data[1])
+                # data = self.transmitter_command_data_queue.get()
+                if from_fastapi_data[0] is None:
                     time.sleep(0.5)
+                    continue
 
-                except socket.error:
-                    time.sleep(0.5)
-                    # client_socket.close()
-                    # print("Socket closed due to error")
-                    # break
+                if isinstance(from_fastapi_data, tuple):
+                    string_data = ' '.join(map(str, from_fastapi_data))
+                    client_socket.sendall(string_data.encode())
+                    print('{} command transmit [{}] from {}'.format(dt.now(), string_data, client_address[0]))
+                else:
+                    print("Something wrong in command")
+
+                time.sleep(0.5)
+
+            except socket.error as e:
+                print("Socket error:", e)
+                time.sleep(0.5)
+
+            except queue.Empty:
+                print("socket_server_queue empty")
+                time.sleep(0.5)
+
+            except Exception as ex:
+                print("An error occurred:", ex)
 
     def put_command_data(self, command, data):
         print("put_command_data")
